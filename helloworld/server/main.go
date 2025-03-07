3808c651a0aa97b80ca57c726c524efbf3221e2f
Bignya Pathi <bignya.pathi@movius.ai>
2025-03-07 12:18:03 +0530

    Adding interceptors in server side

--- helloworld/server/main.go
+++ helloworld/server/main.go
@@ -24,13 +24,50 @@ func (s *server) SayHello(_ context.Context, in *pb.HelloRequest) (*pb.HelloResp
 	return &pb.HelloResponse{Message: "Hello " + in.GetName()}, nil
 }
 
+func loggingInterceptor(
+	ctx context.Context,
+	req any,
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler,
+) (any, error) {
+	fmt.Printf("Calling the Logging Interceptor: %v\n", info.FullMethod)
+	fmt.Printf("User %v is requesting\n", ctx.Value("user"))
+	resp, err := handler(ctx, req)
+	if err != nil {
+		return "", err
+	}
+	return resp, nil
+}
+
+type MyKeyType string
+
+func authInterceptor(
+	ctx context.Context,
+	req any,
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler,
+) (any, error) {
+	fmt.Printf("Calling the Auth Interceptor: %v\n", info.FullMethod)
+	var userKey MyKeyType = "user"
+	resp, err := handler(context.WithValue(ctx, userKey, "Bignya"), req)
+	if err != nil {
+		return "", err
+	}
+	return resp, nil
+}
+
 func main() {
 	flag.Parse()
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
-	s := grpc.NewServer()
+	s := grpc.NewServer(
+		grpc.ChainUnaryInterceptor(
+			authInterceptor,
+			loggingInterceptor,
+		),
+	)
 	pb.RegisterGreeterServer(s, &server{})
 	log.Printf("Sever listening at %v", lis.Addr().String())
 	if err := s.Serve(lis); err != nil {
